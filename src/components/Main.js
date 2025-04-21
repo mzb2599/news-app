87dbdb1ffd26e4afc38562951c12c87d39b3bb6e
mzaki2599 <mzaki2599@gmail.com>
2025-04-21 14:47:29 +0530

    E2E cypress tests added

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,8 +5,8 @@ import CircularProgress from "@mui/material/CircularProgress";
 const Main = ({ newsData, loading = false }) => {
   return (
     <div className="main">
-      <h1>News Articles</h1>
-      {loading && <CircularProgress />}
+      <h1>Most Viewed Articles</h1>
+      {loading && <CircularProgress data-testid="loading-spinner"/>}
       {!loading && newsData && newsData?.length === 0 && (
         <div className="no-data">No data available</div>
       )}
---
+++ src/test/cypress/e2e/spec.cy.js
@@ -0,0 +1,24 @@
+describe("template spec", () => {
+  it("passes", () => {
+    cy.visit("http://localhost:3000/");
+    // Page title check 
+    cy.get("h1").should("contain", "Most Viewed Articles");
+
+    // Check if the mui CircularProgress spinner is displayed
+    cy.get("[data-testid='loading-spinner']").should("exist");
+
+    // Check if the loading spinner is removed after data is loaded
+    cy.get("[data-testid='loading-spinner']").should("not.exist");
+
+    // Check if the articles are displayed
+    cy.get("div[class='news-articles']").should("exist");
+
+    // Check if the articles list is not empty
+    cy.get("div[class='news-articles']").children().should("have.length.greaterThan", 0);
+
+    // Check if the first article image is displayed correctly
+    cy.get(':nth-child(1) > .MuiPaper-root > .MuiCardMedia-root').should("exist");
+
+    // Check if the first article abstract is displayed correctly
+      });
+});
